82f9423fbfc31caa1afb60e58b97c450954f2d00
MiguelGonzalezTiger <miguel@codetiger.co>
2020-06-09 17:06:03 -0400

    Celery running

--- Transitions/main/tasks.py
+++ Transitions/main/tasks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import task
 from main.models import User
 from organizations.models import Organization
+from organizations.backends import invitation_backend
 from celery.contrib import rdb
 import logging
 
@@ -16,11 +17,13 @@ logger.addHandler(sh)
 
 @task(name='invite_user_task')
 def invite_user_task(email, sender_username, *args, **kwargs):
-    # print('from task')
-    logger.log('*************from task**************')
-    rdb.set_trace()
+    print('from task')
+    sender = None
+    if sender_username != '':
+        sender = User.objects.get(username=sender_username)
+    # logger.log(msg='*************from task**************')
     user = invitation_backend().invite_by_email(
             email,
             **{'domain': {'domain': 'localhost:8000'},
                 'organization': Organization.objects.last(),
-                'sender': User.objects.get(username=sender_username)})
+                'sender': sender})
